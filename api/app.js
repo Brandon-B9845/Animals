4f69c0b46d4902ae934fa8b0a68416323e0f84ff
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-02-10 22:02:27 +0100

    added an api route and got some data back onClick

--- api/app.js
+++ api/app.js
@@ -1,3 +1,6 @@
+var cors = require("cors");
+
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,22 +8,28 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var otherRouter = require('./routes/other')
 var usersRouter = require('./routes/users');
+const { normalize } = require('path');
 
 var app = express();
 
 // view engine setup
-app.set('views', '../src/App');
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.set('port', process.env.PORT || 9000)
+app.listen(9000)
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/other', otherRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
