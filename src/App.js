4e676d0675b675342dc4e4853df0d73b03e6766a
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-02-10 19:18:39 +0100

    init app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>What if god was one of us?</p>
+      <ul>
+        <li>a dog like one of us?</li>
+        <li>some frogs like one of us?</li>
+        <li>on a log like one of us?</li>
+      </ul>
+      <h1>THERES A HOLE IN THE BOTTOM OF THE SEA</h1>
+    </div>
+  );
+}
+
+export default App;
