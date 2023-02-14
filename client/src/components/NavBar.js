ae859b0e335aedcbc1ad9572c1cf20eee74b6185
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-02-14 19:50:10 +0100

    did css styling and added in a nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -8,16 +8,11 @@ function NavBar(props) {
             <header className="main-header">
                 <img className="header-img" src={process.env.PUBLIC_URL + 'turkey.jpeg.png'} alt="a turkey" />
                 <h2 className="header-title">Turkish Movie Central</h2>
-                <div classname="nav-btn-container">
-                    <p classname="nav-btn">Home</p>
-                </div>
-                <div classname="nav-btn-container">
-                    <p classname="nav-btn">Random Movie!</p>
-                </div >
-                <div classname="nav-btn-container">
-                    <p classname="nav-btn">About us</p>
-                </div>
-
+                <ul className="nav">
+                    <li>Home</li>
+                    <li>Random Movie!</li>
+                    <li>About Us</li>
+                </ul>
             </header>
         </div>
     );
