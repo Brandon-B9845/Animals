2457163243b4121412aaa72b8faffa5b2dde1bce
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-02-16 22:50:33 +0100

    routed some information to the front end

--- api/routes/pokemon.js
+++ api/routes/pokemon.js
@@ -4,8 +4,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log(db)
-  res.send('respond with a resource');
+  const test = db.Pokemon.findAll({
+  where: {
+    pokedex_number: 5
+  }
+  })
+  test.then(data => console.log(data))
 });
 
 module.exports = router;
