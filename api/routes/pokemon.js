0b7a0b9d438b2da98c473e119492f58074e40682
Brandon Ballesteros <brandonballesteros9845@gmail.com>
2023-02-17 20:36:58 +0100

    2 changes...

--- api/routes/pokemon.js
+++ api/routes/pokemon.js
@@ -4,10 +4,27 @@ var router = express.Router();
 const { Op } = require("sequelize");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  const test = db.Pokemon.findAll()
-   let anotherTest = test.then((data) => {return res.send(data)})
-   console.log(anotherTest)
+
+
+router.get('/legendary', async function(req, res, next) {
+  const test =  await db.Pokemon.findAll({
+    where: {
+      is_legendary: 1,
+    }, 
+    limit: 25 
+  })
+  res.send(test)
+  console.log(req.body)
+  
+  
+});
+
+router.post('/*', async function(req, res, next) {
+  const test = await db.Pokemon.findAll({
+    limit: 25 
+  })
+   res.send(test)
+   console.log(req.body, "here")
 });
 
 module.exports = router;
--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Card from './components/PokeCard';
 import NavBar from './components/NavBar';
 import './App.css';
 
+
 function App() {
 
   const [pokemon, setPokemon] = useState([])
@@ -12,7 +13,19 @@ function App() {
   }, []);
   
   async function handleClick() {
-    let results = await fetch('http://localhost:3000/pokemon')
+    const options = {
+      method: 'POST',
+      headers: {
+          'Content-type': 'application/x-www-form-urlencoded',
+          // 'X-RapidAPI-Host': 'rewriter-paraphraser-text-changer-multi-language.p.rapidapi.com',
+          // 'X-RapidAPI-Key': ''
+          // 'Access-Control-Allow-Origin': '*'
+      },
+      body: "{'language':'en','strength':3,'text':'Hey! Let's learn something together.'}"
+    }
+  
+    
+    let results = await fetch('http://localhost:3000/pokemon', options)
     let returnPokemon = await results.json()
     console.log(returnPokemon)
     setPokemon(returnPokemon)
@@ -28,15 +41,24 @@ function findPicture(pokedex_number){
   
 } 
 
+async function leggyBoyos(){
+  let results = await fetch('http://localhost:3000/pokemon/legendary')
+  let returnPokemon = await results.json()
+  console.log(returnPokemon)
+  setPokemon(returnPokemon)
+}
+
   return (
     <div className="App">
       <header>
         <div>
           <NavBar />
+          <button onClick={leggyBoyos}>AHHHH</button>
+          <button onClick={handleClick} >Click me</button>
           <div className="wrapper">
           {pokemon ? pokemon.map((poke) => <Card key={poke.pokedex_number} picture={poke.pokedex_number < 494 ?  `https://serebii.net/pokearth/sprites/dp/${findPicture(poke.pokedex_number)}.png` : `https://www.serebii.net/swordshield/pokemon/${poke.pokedex_number}.png`} name={poke.name} type={poke.type1} attack={poke.attack} defense={poke.defense}gen={poke.generation} pokedex={poke.pokedex_number} />) : <p>Click the button</p>}
           </div>
-          <button onClick={handleClick} >Click me</button>
+
         </div>
 
       </header>
